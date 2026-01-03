5dc082efdbbdc5ea10c4a62835318a05633013f7
Errorsia <Errorsia@outlook.com>
2026-01-04 07:47:52 +0800

    feat.: new build version system

--- jiv/config/build_config.py
+++ jiv/config/build_config.py
@@ -1,10 +1,33 @@
 PROJECT_NAME = 'Jiyu Immunodeficiency Virus'
-MAJOR_VER, MINOR_VER = 0, 3
-RELEASE_STAGE = 'a2'
-VERSION = f'{MAJOR_VER}.{MINOR_VER}{RELEASE_STAGE}'
-FULL_VERSION = f'{PROJECT_NAME} v{VERSION}'
+
+MAJOR_VER = 0
+MINOR_VER = 3
+PATCH_VER = 0
+
+PRE_STAGE = "a"     # alpha / beta / rc
+PRE_NUM = 2         # a2 / b1 / rc3
+
+# PEP 440 version
+if PRE_STAGE:
+    VERSION = f"{MAJOR_VER}.{MINOR_VER}{PRE_STAGE}{PRE_NUM}"
+else:
+    VERSION = f"{MAJOR_VER}.{MINOR_VER}"
+
+# Windows numeric version
+STAGE_MAP = {"a": 0, "b": 1, "rc": 2, None: 3}
+WIN_FILEVER = (
+    MAJOR_VER,
+    MINOR_VER,
+    STAGE_MAP[PRE_STAGE],
+    PRE_NUM if PRE_STAGE else PATCH_VER
+)
+
+FULL_VERSION = f"{PROJECT_NAME} v{VERSION}"
+
 UPDATE_URL = "https://api.github.com/repos/Errorsia/PJIV-pre/releases/latest"
+
 E_CLASSROOM_NAME = 'studentmain'
 E_CLASSROOM_PROGRAM_NAME = E_CLASSROOM_NAME + '.exe'
+
 # CODE_NAME = ''
 # NICKNAME = ''
