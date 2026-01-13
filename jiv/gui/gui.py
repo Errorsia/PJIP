7d0ecdd97f10f688765044544072f74747e1685d
Errorsia <Errorsia@outlook.com>
2026-01-13 19:20:41 +0800

    feat: InfoPage

--- jiv/gui/gui.py
+++ jiv/gui/gui.py
@@ -682,6 +682,30 @@ class UpdatePage(QWidget, RequireNameMixin):
             self.update_state_label.setText("Unexpected state. Please contact the developers.")
 
 
+class InfoPage(QWidget, RequireNameMixin):
+    ui_change = Signal(str, object)
+
+    def __init__(self):
+        super().__init__()
+        self.page_name = None
+
+        self.init_ui()
+
+    def set_page_name(self):
+        self.page_name = 'Info'
+
+    def init_ui(self):
+        main_layout = QVBoxLayout()
+        # main_layout.setContentsMargins(3, 3, 3, 3)
+        # main_layout.setSpacing(5)
+
+        page_updating = PageUpdating()
+
+        main_layout.addWidget(page_updating)
+
+        self.setLayout(main_layout)
+
+
 class PageUpdating(QWidget):
     def __init__(self):
         super().__init__()
